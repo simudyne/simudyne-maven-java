f6c94465cb2765299433be985dfe7645c0d9827c
michael <michael@simudyne.com>
2019-03-06 15:12:38 +0000

    fix model indeterminacy using model context change pom ready for 2.2

--- src/main/java/org/example/models/trading/TradingModel.java
+++ src/main/java/org/example/models/trading/TradingModel.java
@@ -7,31 +7,34 @@ import simudyne.core.annotations.Constant;
 import simudyne.core.annotations.Input;
 import simudyne.core.annotations.ModelSettings;
 
-import java.util.Random;
-
 @ModelSettings(macroStep = 100)
 public class TradingModel extends AgentBasedModel<TradingModel.Globals> {
 
   public static final class Globals extends GlobalState {
     @Input(name = "Update Frequency")
-    public double updateFrequency = 0.01;
+    double updateFrequency = 0.01;
 
     @Constant(name = "Number of Traders")
-    public long nbTraders = 1000;
+    long nbTraders = 1000;
 
     @Input(name = "Lambda")
-    public double lambda = 10;
+    double lambda = 10;
 
     @Input(name = "Volatility of Information Signal")
-    public double volatilityInfo = 0.001;
+    double volatilityInfo = 0.001;
 
-    public double informationSignal = new Random().nextGaussian() * volatilityInfo;
+    double informationSignal;
   }
 
   {
     createLongAccumulator("buys", "Number of buy orders");
     createLongAccumulator("sells", "Number of sell orders");
     createDoubleAccumulator("price", "Price");
+  }
+
+  @Override
+  public void init() {
+      getGlobals().informationSignal = getContext().getPRNG().generator.nextGaussian()*getGlobals().volatilityInfo;
   }
 
   @Override
@@ -49,7 +52,8 @@ public class TradingModel extends AgentBasedModel<TradingModel.Globals> {
   public void step() {
     super.step();
 
-    getGlobals().informationSignal = new Random().nextGaussian() * getGlobals().volatilityInfo;
+    getGlobals().informationSignal = getContext().getPRNG().generator.nextGaussian()*getGlobals().volatilityInfo;
+
 
     run(Trader.processInformation(), Market.calcPriceImpact(), Trader.updateThreshold());
   }
